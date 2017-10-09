34193a4111257a574da66c4c1ca0ced036544065
Peter Le <Peter Le@LC-PETERLAPTOP.lec.corp.com>
2017-10-09 00:29:20 -0500

    Forgot to Update Homework...

--- javascript/game.js
+++ javascript/game.js
@@ -1,60 +1,63 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
-var guess = [];
+    var wins = 0;
+    var losses = 0;
+    var guessesLeft = 9;
+    var guess = [];
 
-var computerLetter = computerChoices[Math.floor(Math.random() * 26)];
-console.log (computerLetter);
+    var computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log (computerLetter);
 
-var scoreResults =
-"<p>Wins: " + wins + "</p>" +
-"<p>Losses: " + losses + "</p>" +
-"<p>Guesses Left: " + guessesLeft + "</p>" +
-"<p>Your Guesses so far: " + guess +"</p>";
+     var scoreResults =
+  	"<p>Wins: " + wins + "</p>" +
+  	"<p>Losses: " + losses + "</p>" +
+ 	"<p>Guesses Left: " + guessesLeft + "</p>" +
+  	"<p>Your Guesses so far: " + guess +"</p>";
 
-document.querySelector("#game").innerHTML = scoreResults;	
+  	document.getElementById("#game").innerHTML = scoreResults;	
 
-document.onkeyup = function(event) {
+  	document.onkeyup = function(event) {
 
-	var userInput = event.key;
-	if (computerChoices.indexOf(userInput) == -1) {
-		alert("Invalid guess!") 
-	} 
-	else if (guess.indexOf(userInput) !== -1){
-		alert("You have already guessed this letter!");
-	} 
-	else  { guess.push(userInput);
+	  	var userInput = event.key;
+	  	userInput = userInput.toLowerCase();
+	  	if (computerChoices.indexOf(userInput) == -1) {
+	 		alert("Invalid guess!") 
+	 	} 
+	 	else if (guess.indexOf(userInput) !== -1){
+	 		alert("You have already guessed this letter!");
+	 	} 
+
+	 	else  { guess.push(userInput);
 
 
- 	if (userInput != computerChoices) {
- 		guessesLeft--;
+		 	if (userInput != computerChoices) {
+		 		guessesLeft--;
+		  	}
+
+		  	if (computerLetter === userInput) {
+		  		wins++;
+		  		guessesLeft = 9;
+		  		guess = [];
+		  		computerLetter = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+		  		console.log(computerLetter);
+		  	}
+
+
+		  	if (guessesLeft === 0) {
+		  		losses++;
+		  		guessesLeft = 9;
+		  		guess = [];
+		  		computerLetter = computerChoices[Math.floor(Math.random() * 26)];
+		  		console.log(computerLetter);
+		  	}
+	  	}
+
+	     var scoreResults =
+	  	"<p>Wins: " + wins + "</p>" +
+	  	"<p>Losses: " + losses + "</p>" +
+	 	"<p>Guesses Left: " + guessesLeft + "</p>" +
+	  	"<p>Your Guesses so far: " + guess +"</p>";
+
+  	document.getElementById("#game").innerHTML = scoreResults;
+  		  	
   	}
-
-  	if (computerLetter === userInput) {
-  		wins++;
-  		guessesLeft = 9;
-  		guess = [];
-  		computerLetter = computerChoices[Math.floor(Math.random() * 26)];
-  		console.log(computerLetter);
-  	}
-
-
-  	if (guessesLeft === 0) {
-  		losses++;
-  		guessesLeft = 9;
-  		guess = [];
-  		computerLetter = computerChoices[Math.floor(Math.random() * 26)];
-  		console.log(computerLetter);
-  	}
-	}
-
- var scoreResults =
-	"<p>Wins: " + wins + "</p>" +
-	"<p>Losses: " + losses + "</p>" +
-	"<p>Guesses Left: " + guessesLeft + "</p>" +
-	"<p>Your Guesses so far: " + guess +"</p>";
-
-document.querySelector("#game").innerHTML=scoreResults;	  	
-}
